e00bcaceb4b2cbbcecacdea32a6c5886ebfc115c
Shahroz-Codes <159623726+Shahroz-Codes@users.noreply.github.com>
2024-07-19 01:05:32 +0500

    link issue solved

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ const Get_City_Coordinates = () => {
     if (!cityname) {
         return;
     }
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=1&appid=${API_KEY}`
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=1&appid=${API_KEY}`
     fetch(GEOCODING_API_URL)
         .then(Response => {
             return Response.json();
@@ -93,7 +93,7 @@ const Get_User_Coordinates = () => {
     navigator.geolocation.getCurrentPosition(
         position => {
             const { latitude, longitude } = position.coords;
-            const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+            const REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
 
             fetch(REVERSE_GEOCODING_URL)
                 .then(Response => {
